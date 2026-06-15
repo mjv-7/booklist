ea8fcb66a7e65f08b1e4bad35578bf7299beb9e4
mjv-7 <mujibullafikrat@gmail.com>
2026-06-15 02:26:40 -0400

    222

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-mod script_text_file;
-use script_text_file::TextFile;
 /*
 By: Mujibullah
 Date: 2026-04-20
 Program Details: A program where you can add/remove or see books.
 */
-fn save_info(){
-    // Save string data (player names)
-    let names = vec![""];
-    let result = TextFile::save_strings("textfile.txt", names);
-    input("please enter the name of the books: ");
-    if let Err(e) = result {
-        println!("Error saving names: {}", e);
+mod script_text_file;
+use script_text_file::TextFile;
+use std::io;
+
+
+fn main() {
+    save_info();
+    print_data();
+}
+fn save_info() {
+    loop {
+        println!("Enter the name of the book: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("error: please enter a string");
+
+        println!("Would you like to add another book? (yes/no): ");
+        let mut again = String::new();
+        io::stdin().read_line(&mut again).expect("error: please enter a string");
+        if again.trim().to_lowercase() == "no" {
+            break;
+        }
+
+        // Save string data (player names)
+        let names = vec![input.trim()];
+        let result = TextFile::save_strings("textfile.txt", names);
+        if let Err(e) = result {
+            println!("Error saving names: {}", e);
+        }
     }
 }
-fn print_data(){
+fn print_data() {
     // Load player names
     let result = TextFile::load_strings("textfile.txt");
     if let Ok(names) = result {
@@ -24,9 +43,4 @@ fn print_data(){
     } else if let Err(e) = result {
         println!("Error loading names: {}", e);
     }
-}
-fn main() {
-    save_info();
-    print_data();
-    println!("Hello booklist!");
 }
